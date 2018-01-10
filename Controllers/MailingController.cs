c6d123ca66fd2997263da0f32f4fdceb72c918d0
Peter-S <32533746+Peter-S@users.noreply.github.com>
2018-01-10 12:40:47 -0800

    FirstLookFirstCommit

---
+++ Controllers/MailingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstLook.Models;
+
+namespace FirstLook.Controllers
+{
+    public class MailingController : Controller
+    {
+        // GET: Mailing
+        public ActionResult Index()
+        {
+            Mailing m1 = new Mailing();
+            m1.FirstName = "Pete";
+            m1.LastName = "Soukup";
+            m1.Email = "lm@me.com";
+
+            Mailing m2 = new Mailing();
+            m2.FirstName = "Fred";
+            m2.LastName = "Estarir";
+            m2.Email = "l@e.com";
+
+            List<Mailing> mailings = new List<Mailing>();
+            mailings.Add(m1);
+            mailings.Add(m2);
+
+            return View(mailings);
+        }
+    }
+}
